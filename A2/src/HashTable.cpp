1446081cc483e67fd15a5a38eb9ec02181ca9b1f
goldengeneral <ncheng25@my.bcit.ca>
2024-03-04 18:15:53 -0800

    Filled empty commands

--- A2/src/HashTable.cpp
+++ A2/src/HashTable.cpp
@@ -1,3 +1,75 @@
 //
 // Created by goldengeneral on 04/03/24.
 //
+
+#include "../include/HashTable.h"
+#include <string>
+#include <vector>
+#include <limits>
+#include <type_traits>
+
+template <typename KeyType, typename ValueType>
+unsigned int HashTable<KeyType, ValueType>::findFreeSlot(std::vector<Bucket> &cTable, unsigned int startIndex,
+                                                         unsigned int &currentHop) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::rehash() {
+
+}
+
+template <typename KeyType, typename ValueType>
+HashTable<KeyType, ValueType>::HashTable(unsigned int size, double threshold) {
+
+}
+
+template <typename KeyType, typename ValueType>
+typename HashTable<KeyType, ValueType>::Iterator HashTable<KeyType, ValueType>::begin() {
+
+}
+
+template <typename KeyType, typename ValueType>
+typename HashTable<KeyType, ValueType>::Iterator HashTable<KeyType, ValueType>::end() {
+
+}
+
+template <typename KeyType, typename ValueType>
+ValueType &HashTable<KeyType, ValueType>::operator[](const KeyType &key) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::updateValueForKey(const KeyType &key, ValueType newValue) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::insert(const KeyType &key, const ValueType &value) {
+
+}
+
+template <typename KeyType, typename ValueType>
+ValueType *HashTable<KeyType, ValueType>::search(const KeyType &key) {
+
+}
+
+template <typename KeyType, typename ValueType>
+bool HashTable<KeyType, ValueType>::remove(const KeyType &key) {
+
+}
+
+template <typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::clear() {
+
+}
+
+template <typename KeyType, typename ValueType>
+unsigned int HashTable<KeyType, ValueType>::size() const {
+
+}
+
+template <typename KeyType, typename ValueType>
+double HashTable<KeyType, ValueType>::loadFactor() const {
+
+}
