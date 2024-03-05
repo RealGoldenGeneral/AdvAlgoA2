fe9864281eb52a41b8aafa870a44c7ef2da6a6cf
goldengeneral <ncheng25@my.bcit.ca>
2024-03-04 18:27:41 -0800

    Added hash function

--- A2/src/HashTable.cpp
+++ A2/src/HashTable.cpp
@@ -20,6 +20,17 @@ void HashTable<KeyType, ValueType>::rehash() {
 }
 
 template <typename KeyType, typename ValueType>
+unsigned int hash(const KeyType& key, int tableSize) {
+    unsigned int hashVal = 0;
+
+    for (char ch : key) {
+        hashVal = 37 * hashVal + ch;
+    }
+
+    return hashVal % tableSize;
+}
+
+template <typename KeyType, typename ValueType>
 HashTable<KeyType, ValueType>::HashTable(unsigned int size, double threshold) {
 
 }
